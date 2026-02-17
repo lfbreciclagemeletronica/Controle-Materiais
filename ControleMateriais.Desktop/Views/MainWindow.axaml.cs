64b0c9df452bc5fc609dfd528bd0c0b273250795
Gabriel_Stundner <gabriel.stundner@dell.com>
2026-02-17 16:24:22 -0300

    (#1) - First Version

---
+++ ControleMateriais.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ControleMateriais.Desktop.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
